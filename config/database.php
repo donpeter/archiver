589d63f72dbe483d5d41dce49bd6740cf2bf431e
Don Peter C. Atunalu <peter@donpeter.me>
2017-09-25 11:00:43 +0300

    Reset Database for Heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,17 +66,17 @@ return [
             'sslmode' => 'require',
         ],*/
 
-        // 'pgsql' => [
-        //     'driver' => 'pgsql',
-        //     'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-        //     'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-        //     'username' => parse_url(getenv("DATABASE_URL"))["user"],
-        //     'password' => parse_url(getenv("DATABASE_URL"))["pass"],
-        //     'charset' => 'utf8',
-        //     'prefix' => '',
-        //     'schema' => 'public',
-        //     'sslmode' => 'require',
-        // ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'require',
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
